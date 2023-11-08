907e985c7e1e551e62ca405af31fa97dd8bc172f
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2023-11-08 20:55:35 +0530

    Initial commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using Syncfusion.Maui.DataGrid;
+using Syncfusion.Maui.Inputs;
+
+namespace MauiApp1
+{
+
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Delete_All_Button_Clicked(object sender, EventArgs e)
+        {
+            if (viewModel.OrderInfoCollection != null)
+            {
+                viewModel.OrderInfoCollection.Clear();
+            }
+
+        }
+    }
+}
+
+
+
